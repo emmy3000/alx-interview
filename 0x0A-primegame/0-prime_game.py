422d37accab76e1664e25ebbd77d6e6b73f54153
Emeka Emodi <emodiemeka@gmail.com>
2023-12-07 00:05:11 +0100

    feat: Implement Python script for a prime number game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,17 +6,13 @@ Script: 0-prime_game
 
 def is_prime(num):
     """
-    Determine the winner of a series of prime game rounds.
+    Check if a given number is prime.
 
     Args:
-    - x (int): The number of rounds to be played.
-    - nums (List[int]): An array of integers representing
-    the ending values (n) for each round.
+    - num (int): The number to be checked for primality.
 
     Returns:
-    - str or None: The name of the player who won the most rounds
-    (either "Maria" or "Ben"). If the winner cannot be determined, 
-    returns None.
+    - bool: True if the number is prime, False otherwise.
     """
     if num < 2:
         return False
@@ -26,7 +22,29 @@ def is_prime(num):
     return True
 
 def isWinner(x, nums):
+    """
+    Determine the winner of a series of prime game rounds.
+
+    Args:
+    - x (int): The number of rounds to be played.
+    - nums (List[int]): An array of integers representing
+      the ending values (n) for each round.
+
+    Returns:
+    - str or None: The name of the player who won the most rounds
+      (either "Maria" or "Ben"). If the winner cannot be determined,
+      returns None.
+    """
     def get_primes(n):
+        """
+        Generate a list of prime numbers up to n.
+
+        Args:
+        - n (int): The upper limit for generating prime numbers.
+
+        Returns:
+        - List[int]: A list of prime numbers up to n.
+        """
         primes = []
         for i in range(2, n + 1):
             if is_prime(i):
@@ -34,6 +52,18 @@ def isWinner(x, nums):
         return primes
 
     def can_make_move(nums, primes):
+        """
+        Check if a player can make a move in the current round.
+
+        Args:
+        - nums (List[int]): The list of available numbers
+          in the current round.
+        - primes (List[int]): The list of prime numbers
+          for the current round.
+
+        Returns:
+        - bool: True if a move can be made, False otherwise.
+        """
         for p in primes:
             if p in nums:
                 return True
