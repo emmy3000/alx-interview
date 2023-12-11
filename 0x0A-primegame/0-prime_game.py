7c226a08b16b9ba1a1a34f80e61241b921a2a61c
Emeka Emodi <emodiemeka@gmail.com>
2023-12-11 04:23:39 +0100

    feat: Implement Python script for a prime number game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -5,8 +5,17 @@ Script: 0-prime_game
 
 
 def isWinner(x, nums):
-    """x - rounds
-    nums - numbers list
+    """
+    Determine the winner of the prime game for multiple rounds.
+
+    Args:
+    - x (int): The number of rounds to be played.
+    - nums (List[int]): A list of integers representing
+      the upper limits for each round.
+
+    Returns:
+    - str or None: The name of the player who won the most rounds
+    (either "Maria", "Ben") or None if it's a tie.
     """
     if x <= 0 or nums is None:
         return None
@@ -16,29 +25,40 @@ def isWinner(x, nums):
     ben = 0
     maria = 0
 
-    a = [1 for x in range(sorted(nums)[-1] + 1)]
-    a[0], a[1] = 0, 0
-    for i in range(2, len(a)):
-        remove_multiples(a, i)
+    primes = [1 for _ in range(sorted(nums)[-1] + 1)]
+    primes[0], primes[1] = 0, 0
+
+    for i in range(2, len(primes)):
+        remove_multiples_of_prime(primes, i)
 
     for i in nums:
-        if sum(a[0:i + 1]) % 2 == 0:
+        if sum(primes[0:i + 1]) % 2 == 0:
             ben += 1
         else:
             maria += 1
+
     if ben > maria:
         return "Ben"
-    if maria > ben:
+    elif maria > ben:
         return "Maria"
-    return None
+    else:
+        return None
 
 
-def remove_multiples(ls, x):
-    """removes multiple
-    of primes
+def remove_multiples_of_prime(primes, prime):
     """
-    for i in range(2, len(ls)):
+    Function modifies the input list to remove multiples
+    of the specified prime number.
+
+    Args:
+    - primes (List[int]): The list of numbers to remove multiples from.
+    - prime (int): The prime number whose multiples need to be removed.
+
+    Returns:
+    - None
+    """
+    for i in range(2, len(primes)):
         try:
-            ls[i * x] = 0
+            primes[i * prime] = 0
         except (ValueError, IndexError):
             break
